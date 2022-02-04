ad7119f09fd83cddc977aabdb2fc93d79037748b
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-02-04 21:06:41 +0100

    Add vigenere ciphering

--- ciphers.py
+++ ciphers.py
@@ -1,21 +1,34 @@
-fileToCipherName = "textToCipherWithCaesarCipher.txt"
-fileToDecipherName = "textToDecipherWithCaesarCipher.txt"
+fileToCipherName = "textToCipher.txt"
+fileToDecipherWithCaesarCipherName = "textToDecipherWithCaesarCipher.txt"
+fileToDecipherWithVigenereCipherName = "textToDecipherWithVigenereCipher.txt"
 englishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 polishAlphabet = "AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ"
 
 
 def readFile(filename):
     with open(filename, "r", encoding="utf-8") as inFile:
-        text = inFile.readlines()
+        text = inFile.read().upper()
     return text
 
 def caesarCipher(text, shift, alphabet):
     shiftedAlphabed = alphabet[shift:] + alphabet[:shift]
     table = str.maketrans(alphabet, shiftedAlphabed)
-    return str(text).translate(table)
+    return text.translate(table)
+
+def vigenereCipher(text, keyword, alphabet):
+    cipheredText = ""
+    numberOfOtherCharacters = 0
+    for (characterNumber, character) in enumerate(text):
+        if character in alphabet:
+            cipheredText += alphabet[(alphabet.index(character) + alphabet.index(keyword[(characterNumber - numberOfOtherCharacters) % len(keyword)])) % len(alphabet)]
+        else:
+            cipheredText += character
+            numberOfOtherCharacters += 1
+    return cipheredText
 
 if __name__ == '__main__':
     textToCipher = readFile(fileToCipherName)
     print(caesarCipher(textToCipher, -3, englishAlphabet))
-    textToDecipher = readFile(fileToDecipherName)
+    textToDecipher = readFile(fileToDecipherWithCaesarCipherName)
     print(caesarCipher(textToDecipher, 3, englishAlphabet))
+    print(vigenereCipher(textToCipher, "LION", englishAlphabet))
