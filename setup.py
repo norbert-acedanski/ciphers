690988f95c0d07e5894a7c4168eb798b9c2f804d
Norbert Acedanski <norbert.acedanski@gmail.com>
2024-10-26 21:55:25 +0200

    Extend setup.py file.

--- setup.py
+++ setup.py
@@ -1,3 +1,31 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+VERSION = "0.0.1"
+DESCRIPTION = "A ciphers package"
+LONG_DESCRIPTION = "A package that makes it easy to play around with different ciphering and deciphering algorithms"
+
+setup(
+    name="ciphers",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Norbert Acedański",
+    author_email="norbert.acedanski@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    keywords="ciphers",
+    classifiers=[
+        "Development Status :: 1 - Production/Stable",
+        "Framework :: tox",
+        "Intended Audience :: Users",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
+    ]
+)
